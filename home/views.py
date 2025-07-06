fa3c63d426d8cad1cb85715b1e831fb5499fb692
root <root@DESKTOP-HQ6GDB5.localdomain>
2025-07-06 19:17:55 -0300

    Atualizações na página de login

--- home/views.py
+++ home/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Usuario
+from django.shortcuts import redirect
+
 
 def home(request):
-    if request.method == "GET":
+    if request.method == "POST":
         return render(request, 'home.html')
     else:
-        nome = request.POST.get('nome')
-        return HttpResponse(f"Olá, {nome}!")
+        # Se for um POST para a rota home, redireciona para o cadastro
+        return redirect('/cadastro/')
 
 def cadastro(request):
     if request.method == "POST":
@@ -17,21 +19,11 @@ def cadastro(request):
         senha = request.POST.get("senha")
         confirmar = request.POST.get("confirmar_senha")
 
-        # Validações
         if senha != confirmar:
             return HttpResponse("As senhas não coincidem!")
 
-        if Usuario.objects.filter(login=login).exists():
-            return HttpResponse("Esse login já está em uso!")
-
-        if Usuario.objects.filter(email=email).exists():
-            return HttpResponse("Esse e-mail já está cadastrado!")
-
-        # Salvar no banco
-        usuario = Usuario(nome=nome, login=login, email=email, senha=senha)
-        usuario.save()
-        return redirect('/login/')
-
+        Usuario.objects.create(nome=nome, login=login, email=email, senha=senha)
+        return redirect('/login/')  # Redireciona após cadastro
     return render(request, "cadastro.html")
 
 def login_view(request):
@@ -43,6 +35,6 @@ def login_view(request):
             usuario = Usuario.objects.get(login=login, senha=senha)
             return HttpResponse(f"Bem-vindo, {usuario.nome}!")
         except Usuario.DoesNotExist:
-            return HttpResponse("Login ou senha incorretos!")
+            return HttpResponse("Login ou senha incorretos.")
 
     return render(request, "login.html")
--- projeto_koiti/urls.py
+++ projeto_koiti/urls.py
@@ -17,9 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from home.views import home, cadastro
+from home.views import home, cadastro, login_view
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("home/", home),
-    path("cadastro/", cadastro),  # nova rota
+    path("cadastro/", cadastro),
+    path("login/", login_view),  # nova rota
 ]
+
