361831b60000bcad1f3c3ae2c646d6960b61103d
root <root@DESKTOP-HQ6GDB5.localdomain>
2025-07-06 23:32:59 -0300

    Fazendo configurações do Vercel.

--- projeto_koiti/settings.py
+++ projeto_koiti/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -121,3 +122,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ALLOWED_HOSTS = ['https://projeto-do-koiti-faculdade-c69t0s6wg-jhoanns-projects.vercel.app/', 'localhost', '127.0.0.1']
+
+DEBUG = False
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # Onde o Django vai coletar os arquivos estáticos
